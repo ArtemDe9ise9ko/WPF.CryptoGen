552b0b5052686a5700a256e5e22bdbc07b8ecb3e
artden <artden966@gmail.com>
2023-04-28 02:30:48 +0300

    5 - styling plotMode/lpre refactor

--- WPF.CryptoGen.Client/ViewModels/CryptocurrencyViewModel.cs
+++ WPF.CryptoGen.Client/ViewModels/CryptocurrencyViewModel.cs
@@ -1,48 +1,112 @@
 ﻿using OxyPlot;
+using OxyPlot.Annotations;
 using OxyPlot.Axes;
 using OxyPlot.Series;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
+using System;
+using System.Drawing;
+using System.Linq;
 
 namespace WPF.CryptoGen.Client.ViewModels
 {
     public class CryptocurrencyViewModel : ViewModelBase
     {
         public PlotModel PlotModel { get; set; }
+        public PlotController PlotController { get;}
 
         public CryptocurrencyViewModel()
         {
-            // Create the plot model and add the X and Y axes
+            ColorConverter converter = new ColorConverter();
+            Color textColor = (Color)converter.ConvertFromString(System.Windows.Application.Current.FindResource("TextColor").ToString());
+            Color lineSeriesColor = (Color)converter.ConvertFromString(System.Windows.Application.Current.FindResource("LineSeriesColor").ToString());
+            Color areaSeriesColor = (Color)converter.ConvertFromString(System.Windows.Application.Current.FindResource("AreaSeriesColor").ToString());
+
+            LinearAxis axisLeft = new LinearAxis();
+            axisLeft.Minimum = 0;
+            axisLeft.Position = AxisPosition.Left;
+            axisLeft.TextColor = OxyColor.FromRgb(textColor.R, textColor.G, textColor.B);
+            axisLeft.TicklineColor = OxyColor.FromRgb(textColor.R, textColor.G, textColor.B);
+
+            LinearAxis axisBottomSeries = new LinearAxis();
+            axisBottomSeries.Minimum = 0;
+            axisBottomSeries.Position = AxisPosition.Bottom;
+            axisBottomSeries.TextColor = OxyColor.FromRgb(textColor.R, textColor.G, textColor.B);
+            axisBottomSeries.TicklineColor = OxyColor.FromRgb(textColor.R, textColor.G, textColor.B);
+
+            var areaSeries = new AreaSeries()
+            {
+                Color = OxyColor.FromRgb(lineSeriesColor.R, lineSeriesColor.G, lineSeriesColor.B),
+                Fill = OxyColor.FromRgb(areaSeriesColor.R, areaSeriesColor.G, areaSeriesColor.B),
+                Color2 = OxyColors.Transparent
+            };
+
+            var r = new Random(1);
+            var dailyOutput = 0;
+
+            for (int i = 0; i <= 80; i++)
+            {
+                areaSeries.Points.Add(new DataPoint(i, dailyOutput));
+
+                dailyOutput = Math.Max(0, dailyOutput + r.Next(-222, 223));
+            }
+
+            var lastDataPoint = areaSeries.Points.Last();
+            var maxDataPointY = areaSeries.Points.Max(x => x.Y);
+
+            var horizontalLineAnnotation = new LineAnnotation
+            {
+                Y = lastDataPoint.Y,
+                X = lastDataPoint.X,
+                Type = LineAnnotationType.Horizontal,
+                LineStyle = LineStyle.Dot,
+                StrokeThickness = 1,
+                Color = OxyColor.FromRgb(lineSeriesColor.R, lineSeriesColor.G, lineSeriesColor.B),
+                TextHorizontalAlignment = HorizontalAlignment.Right,
+                TextVerticalAlignment = VerticalAlignment.Bottom,
+            };
+
+            var scatterSeries = new ScatterSeries
+            {
+                MarkerType = MarkerType.Circle,
+                MarkerSize = 4,
+                MarkerFill = OxyColor.FromRgb(lineSeriesColor.R, lineSeriesColor.G, lineSeriesColor.B),
+                DataFieldX = "X",
+                DataFieldY = "Y"
+            };
+
+            scatterSeries.Points.Add(new ScatterPoint(lastDataPoint.X, lastDataPoint.Y));
+
+            var annotation = new TextAnnotation
+            {
+                Text = string.Format("{0:F2}", maxDataPointY),
+                TextHorizontalAlignment = HorizontalAlignment.Left,
+                TextVerticalAlignment = VerticalAlignment.Bottom,
+                TextPosition = new DataPoint(axisLeft.ActualMinimum + 1.5, lastDataPoint.Y),
+                Background = OxyColor.FromRgb(areaSeriesColor.R, areaSeriesColor.G, areaSeriesColor.B),
+                TextColor = OxyColor.FromRgb(textColor.R, textColor.G, textColor.B),
+            };
+
+            PlotController = new PlotController();
+            PlotController.UnbindMouseDown(OxyMouseButton.Left);
+            PlotController.UnbindMouseDown(OxyMouseButton.Right);
+            PlotController.BindMouseDown(OxyMouseButton.Left, PlotCommands.SnapTrack);
+            PlotController.BindMouseDown(OxyMouseButton.Right, PlotCommands.PanAt);
+
             PlotModel = new PlotModel();
 
             PlotModel.PlotAreaBorderThickness = new OxyThickness(1,0,0,1);
-            PlotModel.PlotAreaBorderColor = OxyColors.Purple;
+            PlotModel.PlotAreaBorderColor = OxyColor.FromRgb(textColor.R, textColor.G, textColor.B);
 
-            LinearAxis axisLeftSeries = new LinearAxis();
-            axisLeftSeries.Position = AxisPosition.Left;
-            axisLeftSeries.TextColor = OxyColors.Black;
-            axisLeftSeries.TicklineColor = OxyColors.Purple;
-            axisLeftSeries.TitleColor = OxyColors.Purple;
+            PlotModel.Annotations.Add(horizontalLineAnnotation);
+            PlotModel.Annotations.Add(annotation);
 
-            PlotModel.Axes.Add(axisLeftSeries);
-
-            LinearAxis axisBottomSeries = new LinearAxis();
-            axisBottomSeries.Position = AxisPosition.Bottom;
-            axisBottomSeries.TextColor = OxyColors.Black;
-            axisBottomSeries.TicklineColor = OxyColors.Purple;
-            axisBottomSeries.TitleColor = OxyColors.Purple;
-
+            PlotModel.Axes.Add(axisLeft);
             PlotModel.Axes.Add(axisBottomSeries);
 
-            LineSeries lineSeries = new LineSeries();
-            lineSeries.Color = OxyColor.FromAColor(200, OxyColors.Green);
-            lineSeries.Points.Add(new DataPoint(0, 0));
-            lineSeries.Points.Add(new DataPoint(1, 1));
-            lineSeries.Points.Add(new DataPoint(2, 0.5));
-            lineSeries.Points.Add(new DataPoint(3, 1.5));
-            lineSeries.Points.Add(new DataPoint(4, 2.5));
+            ((LinearAxis)PlotModel.Axes[0]).Maximum = maxDataPointY + 5;
+            ((LinearAxis)PlotModel.Axes[1]).Maximum = lastDataPoint.X + 3;
 
-            PlotModel.Series.Add(lineSeries);
+            PlotModel.Series.Add(areaSeries);
+            PlotModel.Series.Add(scatterSeries);
         }
     }
 }
